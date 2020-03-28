891eff97d0b6f278201b32bd838335f39a38a8ac
Francesco <fabadraun@gmail.com>
2020-03-28 13:38:51 +1300

    Remove previous testing components

--- src/components/layout.js
+++ src/components/layout.js
@@ -10,7 +10,6 @@ import PropTypes from "prop-types"
 import { useStaticQuery, graphql } from "gatsby"
 
 import Header from "./header"
-import Nav from "./Nav"
 import "./layout.css"
 
 const Layout = ({ children }) => {
@@ -27,21 +26,7 @@ const Layout = ({ children }) => {
   return (
     <>
       <Header siteTitle={data.site.siteMetadata.title} />
-      <Nav></Nav>
-      <div
-        style={{
-          margin: `0 auto`,
-          maxWidth: 960,
-          padding: `0 1.0875rem 1.45rem`,
-        }}
-      >
-        <main>{children}</main>
-        <footer>
-          © {new Date().getFullYear()}, Built with
-          {` `}
-          <a href="https://www.gatsbyjs.org">Gatsby</a>
-        </footer>
-      </div>
+      <main>{children}</main>
     </>
   )
 }
