b1c85ca6626b52b720fafaec7eccd1be73365261
Francesco <fabadraun@gmail.com>
2020-03-28 13:39:07 +1300

    Add new demo for maxWidth

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,20 +1,15 @@
 import React from "react"
-import { Link } from "gatsby"
+import { Box, Flex, Text } from "@primer/components"
 
 import Layout from "../components/layout"
-import Image from "../components/image"
-import SEO from "../components/seo"
 
 const IndexPage = () => (
   <Layout>
-    <SEO title="Home" />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-      <Image />
-    </div>
-    <Link to="/page-2/">Go to page 2</Link>
+    <Flex justifyContent="center">
+      <Box bg="blue.2" height="400px" width={1} maxWidth="medium">
+        <Text>This box uses maxWidth</Text>
+      </Box>
+    </Flex>
   </Layout>
 )
 
